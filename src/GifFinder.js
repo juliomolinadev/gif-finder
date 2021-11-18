597a5efbec09a9eb05195b3c5b65bd6088dc1e3d
julio <jmolina.dex@gmail.com>
2021-11-18 14:27:40 -0800

    Add search bar

--- src/GifFinder.js
+++ src/GifFinder.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchBar from "./components/SearchBar";
 
 export const GifFinder = () => {
+	const [word, setWord] = useState("");
+
 	return (
-		<div>
-			<h1>Gif Finder</h1>
+		<div className="container">
+			<div className="d-flex justify-content-center mt-5">
+				<h1>Gif Finder</h1>
+			</div>
+
+			<div className="row d-flex justify-content-center mt-4">
+				<div className="col-md-6">
+					<SearchBar setWord={setWord} />
+					<p>Buscando... {word}</p>
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const SearchBar = ({ setWord }) => {
+	const [value, setValue] = useState("");
+
+	const handleInputChange = (e) => {
+		setValue(e.target.value);
+	};
+
+	const handleSetWord = (e) => {
+		e.preventDefault();
+		setWord(value);
+		setValue("");
+	};
+
+	return (
+		<form className="d-flex" onSubmit={handleSetWord}>
+			<input
+				id="searchInput"
+				type="text"
+				value={value}
+				onChange={handleInputChange}
+				className="form-control"
+			/>
+
+			<button className="btn btn-primary ms-2">Buscar</button>
+		</form>
+	);
+};
+
+SearchBar.propTypes = {
+	setWord: PropTypes.func.isRequired
+};
+
+export default SearchBar;
